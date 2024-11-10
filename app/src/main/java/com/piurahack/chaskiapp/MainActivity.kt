25271a6443a98d2b60691d2fa2d645c2fd8ca52e
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2024-11-10 02:53:51 -0500

    principal menu
    bottom navigation

--- app/src/main/java/com/piurahack/chaskiapp/MainActivity.kt
+++ app/src/main/java/com/piurahack/chaskiapp/MainActivity.kt
@@ -1,20 +1,102 @@
 package com.piurahack.chaskiapp
 
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.piurahack.chaskiapp.databinding.ActivityMainBinding
+import com.piurahack.chaskiapp.Fragments.Map
+import com.piurahack.chaskiapp.Fragments.Community
+import com.piurahack.chaskiapp.Fragments.Newspaper
+import com.piurahack.chaskiapp.Fragments.Profile
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.getRoot())
+        replaceFragment(Map())
+        binding!!.bottomNavigationView.setSelectedItemId(R.id.map)
+
+        binding!!.bottomNavigationView.setBackground(null)
+        binding!!.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.getItemId()) {
+                R.id.community -> replaceFragment(Community())
+                R.id.newspapper -> replaceFragment(Newspaper())
+                R.id.map -> replaceFragment(Map())
+                R.id.profile -> replaceFragment(Profile())
+            }
+            true
         }
+
+        binding!!.fab.setOnClickListener(View.OnClickListener { showBottomDialog() })
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun showBottomDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottom_sheet_layout)
+
+        val videoLayout = dialog.findViewById<LinearLayout>(R.id.layoutVideo)
+        val shortsLayout = dialog.findViewById<LinearLayout>(R.id.layoutShorts)
+        val liveLayout = dialog.findViewById<LinearLayout>(R.id.layoutLive)
+        val cancelButton = dialog.findViewById<ImageView>(R.id.cancelButton)
+
+        videoLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(
+                this@MainActivity,
+                "Upload a Video is clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        shortsLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(
+                this@MainActivity,
+                "Create a short is Clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        liveLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(
+                this@MainActivity,
+                "Go live is Clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        cancelButton.setOnClickListener { dialog.dismiss() }
+
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
     }
 }
